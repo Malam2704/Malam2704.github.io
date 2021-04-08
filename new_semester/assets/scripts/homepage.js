171332ca82ab3f278f929eb53177d1b7e64b8549
Mohammed Alam <ma3655@rit.edu>
2021-04-08 17:54:33 -0400

    updated for commits

---
+++ new_semester/assets/scripts/homepage.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var element = document.body;
+    // Changes body color to blakc and text color to black
+    element.style.backgroundColor = "black";
+    element.style.color = "	#C0C0C0";
+}
+
+function addTextBox() {
+    document.getElementById['otherEmailSubject'].style = "visibility: visible;";
+}
+
+function backHome(){
+    window.location.href = "https://people.rit.edu/ma3655/ISTE240/";
+}
