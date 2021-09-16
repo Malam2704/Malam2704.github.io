ca6d2b7924b9b1f5e462526a5c9da1b8cadc9880
Mohammed Alam <ma3655@rit.edu>
2021-09-16 00:01:39 -0400

    Nav-button X

--- js/index.js
+++ js/index.js
@@ -1,18 +1,33 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
+var navToggleState = true
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
+    if (navToggleState == true) {
+        document.getElementById('navthreebars').innerHTML = "&#x2715;";
+        navToggleState = false;
+    } else {
+        document.getElementById('navthreebars').innerHTML = "&#9776;";
+        navToggleState = true;
+    }
 });
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+//     document.getElementById('navthreebars').innerHTML = "&#x2715;";
+// });
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
+        document.body.classList.toggle('nav-open');
+        document.getElementById('navthreebars').innerHTML = "&#9776;";
     })
 })
 
 function close_window() {
     if (confirm("Close Window?")) {
         close();
+    } else {
+        location.reload();
     }
 }
